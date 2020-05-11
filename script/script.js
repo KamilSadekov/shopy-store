a67d093d6128eecbec31a7bbb0a98400e8bc283d
Kamil Sadekov <50349496+KamilSadekov@users.noreply.github.com>
2020-05-11 23:29:13 +0300

    started new page

--- script/script.js
+++ script/script.js
@@ -23,3 +23,25 @@ window.onload = function () {
         }
     });
 };
+
+thumbs.onclick = function (event) {
+    let thumbnail = event.target.closest('a');
+
+    if (!thumbnail) return;
+    showThumbnail(thumbnail.href, thumbnail.title);
+    event.preventDefault();
+}
+
+function showThumbnail(href, title) {
+    largeImg.src = href;
+    largeImg.alt = title;
+}
+
+$(function () {
+    $(".images-item").click(function (e) {
+        e.preventDefault();
+        $(".images-item").removeClass('active-image');
+        $(this).addClass('active-image');
+    });
+});
+
